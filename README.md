7221917279a5bbcfdf6492fb1e17f8097eb9eefc
jo-turn <94jhgmsg@gmail.com>
2020-12-28 04:26:07 +0900

    #3.4done

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 2020 1227 01:14 Twitter clone coding begins
 
 2020 1227 02:41 firebase, router basic setup completed
+
+2020 1228 04:25 #3.4 done.
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,59 @@
-import React from 'react'
+import Nweet from 'components/Nweet'
+import { dbService } from 'myBase'
+import React, { useEffect, useState } from 'react'
 
-const Home = () => <span>Home</span>
+const Home = ({ userObj }) => {
+  // console.log(userObj)
+  const [nweet, setNweet] = useState('')
+  const [nweets, setNweets] = useState([])
+
+  useEffect(() => {
+    dbService.collection('nweets').onSnapshot(snapshot => {
+      const nweetArray = snapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data(),
+      }))
+      setNweets(nweetArray)
+    })
+  }, [])
+  const onSubmit = async e => {
+    e.preventDefault()
+    await dbService.collection('nweets').add({
+      text: nweet,
+      createdAt: Date.now(),
+      creatorId: userObj.uid,
+    })
+    setNweet('')
+  }
+  const onChange = e => {
+    const {
+      target: { value },
+    } = e
+    setNweet(value)
+  }
+  // console.log(nweets)
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={nweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Nweet" />
+      </form>
+      <div>
+        {nweets.map(nweet => (
+          <Nweet
+            key={nweet.id}
+            nweetObj={nweet}
+            isOwner={nweet.creatorId === userObj.id}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
 export default Home
